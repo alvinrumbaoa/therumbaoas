d31302fe975140e6322ba3687650c9cdbbaaaa19
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-09-01 13:48:59 -0700

    sd

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbName = "photoDB";
 
-mongoose.connect("mongodb+srv://xenofy:Senpou_23@photodb.useel.mongodb.net/photoDb?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://xenofy:Senpou_23@photodb.useel.mongodb.net/" + dbName,
 { 
         useNewUrlParser: true,
         useUnifiedTopology: true
