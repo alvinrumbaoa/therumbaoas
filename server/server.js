62becbcc69d6f0b855306665acc4c65a5846693c
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-09-01 01:16:14 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = 8000;   
+const dotenv = require("dotenv");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -10,6 +11,7 @@ require("./config/mongoose.config.js"); //  connecting to mongoose
 require("./routes/blog.routes")(app);
 require("./routes/customer.routes")(app);
 require("./routes/image.routes")(app);
+
 
 
 app.listen(port , () => console.log("Connected to  localhost " + port ));
