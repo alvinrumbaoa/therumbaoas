5ca7750d0b0c7b952f8eab6e139884c39ed6bf33
Alvin Flores Rumbaoa <79444246+xenodochy23@users.noreply.github.com>
2021-09-13 20:36:29 -0700

    save

--- client/src/components/DeleteBlogs.js
+++ client/src/components/DeleteBlogs.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import axios from 'axios';
-import {navigate} from '@reach/router';
+import { navigate } from '@reach/router';
 
-const DeleteBlogs = (props) =>{
-    const {id , afterDelete} = props;
-    
-    const BlogdeleteHandler = (e,id) =>{
-        
-    e.preventDefault();
-    axios.delete("http://the-rumbaoas.herokuapp.com/api/blogs/" + id)
-    .then((res) =>{
-        console.log(res.data);
-        afterDelete(id);
-        navigate("/admin");
-    })
-    .catch((err) => {
-        console.log(err);
-    });   
-}
-return(
-    <div>
-        <button className="red-btn"  onClick={(e) => BlogdeleteHandler(e,id )}>Remove Blog Post</button>
-    </div>
+const DeleteBlogs = (props) => {
+    const { id, afterDelete } = props;
 
-)
+    const BlogdeleteHandler = (e, id) => {
+
+        e.preventDefault();
+        axios.delete("http://localhost:3000/api/blogs/" + id)
+            .then((res) => {
+                console.log(res.data);
+                afterDelete(id);
+                navigate("/admin");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    return ( <
+        div >
+        <
+        button className = "red-btn"
+        onClick = {
+            (e) => BlogdeleteHandler(e, id) } > Remove Blog Post < /button> <
+        /div>
+
+    )
 }
 
 
--- client/src/components/DeleteImage.js
+++ client/src/components/DeleteImage.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import axios from 'axios';
-import {navigate} from '@reach/router';
+import { navigate } from '@reach/router';
 
-const DeleteImage = (props) =>{
-    const {id , afterDelete} = props;
-    
-    const ImagedeleteHandler = (e,id) =>{
-        
+const DeleteImage = (props) => {
+    const { id, afterDelete } = props;
+
+    const ImagedeleteHandler = (e, id) => {
+
         e.preventDefault();
-    axios.delete("http://the-rumbaoas.herokuapp.com/api/images/" + id)
-    .then((res) =>{
-        console.log(res.data);
-        afterDelete(id);
-        navigate("/admin");
-    })
-    .catch((err) => {
-        console.log(err);
-    });   
-}
-return(
-    <div>
-        <button className="red-btn-image"  onClick={(e) => ImagedeleteHandler(e,id )}>Delete Image</button>
-    </div>
+        axios.delete("http://localhost:3000/api/api/images/" + id)
+            .then((res) => {
+                console.log(res.data);
+                afterDelete(id);
+                navigate("/admin");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    return ( <
+        div >
+        <
+        button className = "red-btn-image"
+        onClick = {
+            (e) => ImagedeleteHandler(e, id) } > Delete Image < /button> <
+        /div>
 
-)
+    )
 }
 
 
