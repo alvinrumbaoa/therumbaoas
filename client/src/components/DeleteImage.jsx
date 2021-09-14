538785cee2293099496cae70a70b1e62204f1428
Alvin Flores Rumbaoa <79444246+xenodochy23@users.noreply.github.com>
2021-09-13 21:42:13 -0700

    to jsx

---
+++ client/src/components/DeleteImage.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import { navigate } from '@reach/router';
+
+const DeleteImage = (props) => {
+    const { id, afterDelete } = props;
+
+    const ImagedeleteHandler = (e, id) => {
+
+        e.preventDefault();
+        axios.delete("http://localhost:3000/api/api/images/" + id)
+            .then((res) => {
+                console.log(res.data);
+                afterDelete(id);
+                navigate("/admin");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    return (
+        <div>
+        <button className = "red-btn-image"
+        onClick = {
+            (e) => ImagedeleteHandler(e, id) } > Delete Image </button> 
+        </div>
+
+    )
+}
+
+
+export default DeleteImage;
---
+++ client/src/components/Portfolio.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Navbar from './Navbar';
+import {motion } from 'framer-motion';
+import { Link } from '@reach/router';
+
+const Portfolio= (props) => {
+        
+        const sectionStyling1={
+                backgroundImage: "url('images/event1.jpg')",
+                objectFit: "cover",
+        }
+        const sectionStyling2={
+                backgroundImage: "url('images/maica1.jpg')",
+                objectFit: "cover",
+        }
+        const sectionStyling3={
+                backgroundImage: "url('images/newborn.jpg')",
+                objectFit: "cover",
+        }
+        const sectionStyling4={
+                backgroundImage: "url('images/wedding1.jpg')",
+                objectFit: "cover",
+        }
+
+        const sectionStyling5={
+                backgroundImage: "url('images/food1 (1).jpg')",
+                objectFit: "cover",
+        }
+        return (
+                <div className="portfolio-container">
+                        <Navbar/>
+                                <motion.div animate={{ scale: 1 }} transition={{ duration: 0.5 }}>
+                                <h1 className="big-text">Portfolio</h1>     
+                                <div className="category-container">
+                                        <Link to="/portfolio/events" className="link">
+                                        <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+                                                <div className="cards" style={sectionStyling1}>
+                                                <h2>Events</h2>
+                                                </div>
+                                        </motion.div>
+                                        </Link>
+                                        <Link to="/portfolio/modeling" className="link">
+                                        <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+                                                <div className="cards" style={sectionStyling2}>
+                                                <h2>Models</h2>
+                                                </div>
+                                        </motion.div>
+                                
+                                        </Link>
+                                        <Link to="/portfolio/newborn" className="link">
+                                        <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+                                                <div className="cards" style={sectionStyling3}>
+                                                <h2>Newborn</h2>
+                                                </div>
+                                        </motion.div>
+                                        </Link>
+                                        <Link to="/portfolio/wedding" className="link">
+                                        <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+                                                <div className="cards" style={sectionStyling4} >
+                                                <h2>Wedding</h2>
+                                                </div>
+                                        </motion.div>
+                                        </Link>
+                                        <Link to="/portfolio/food" className="link">
+                                        <motion.div  whileHover={{ scale: 1.1 }}  whileTap={{ scale: 0.9 }} >
+                                                <div className="cards" style={sectionStyling5}>
+                                                <h2>Food</h2>
+                                                </div>
+                                        </motion.div>
+                                        </Link>
+                                </div>
+                                </motion.div>
+                </div>
+        )
+}
+
+export default Portfolio;
