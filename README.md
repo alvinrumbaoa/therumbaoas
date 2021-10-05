a59f26b46767a9f8378274c93c2fe033d2d15c5c
Alvin Rumbaoa <alvinrumbaoa@gmail.com>
2021-10-05 08:29:05 -0700

    about page changed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # therumbaoas
+
+
+Photography Portfolio for Maica Rumbaoa
+
+Overview: Able to See High-Res Photo and view gallery at Homepage
+
+
+Using MERN Stack we can Add Blog Post, View Customer Inquiries ,  Delete and Upload Photos for Homepage throug an Admin Dashboard Page.
+
+
+More Features Coming. 
--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,7 +11,7 @@ const About = (props) =>{
             <div className="wrapper">
                 <div className="left">
                     <h2 className="main-text">About Me</h2> 
-                    <img className="portrait" src="http://placehold.it/" alt="my pic"/>
+                    <img className="aboutmepic2" src="/images/maica3.jpg" alt="my pic" width="200" height="200"/>
                     <h4 className="info">"Me and my husband bring this idea and talent to bring a great look at your memories that will look up for many years. We serviced Orange County are but also flexible to any county around SoCal. We do photgraphy for Newborn, Maternity, Weddings and other. If you are ready to work with us. <Link to="/contactus">Let's get started"</Link></h4>
                     
                 </div>    
